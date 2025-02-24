dafcfdf58308a409fd0babca32a0658f6e94f1a4
kirkseytc <kirkseytc@gmail.com>
2025-02-23 21:23:32 -0500

    3:43:00 commit

--- base/views.py
+++ base/views.py
@@ -76,15 +76,15 @@ def home(request):
     )
 
     topics = Topic.objects.all()
-
     room_count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
-    context = {'rooms': rooms, 'topics': topics, 'room_count':room_count}
+    context = {'rooms': rooms, 'topics': topics, 'room_count':room_count, 'room_messages':room_messages}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    room_m = room.message_set.all().order_by('-created')
+    room_m = room.message_set.all()
     participants = room.particpants.all()
     if request.method == 'POST':
         messages = Message.objects.create(
