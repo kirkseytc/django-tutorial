13b3ae014850d008ae8ebddf940b4f1fe230043a
kirkseytc <kirkseytc@gmail.com>
2025-03-09 20:46:50 -0400

    5:35:02 commit

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 
 # Create your views here.
@@ -92,7 +92,7 @@ def room(request, pk):
             room=room,
             body=request.POST.get('body')
         )
-        room.particpants.add(request.user)
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
 
     context = {'room':room, 'room_m':room_m, 'participants': participants}
@@ -169,3 +169,16 @@ def deleteMessage(request, pk):
 
     context = {'obj': msg}
     return render(request, 'base/delete.html', context)
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+
+    return render(request, 'base/update_user.html', {'form': form})
